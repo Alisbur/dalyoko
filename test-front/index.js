cd854035ba4c866c617800523584700045766800
Alisbur <mail-4-eugen@yandex.ru>
2025-03-08 02:18:34 +0300

    fix: small issues

--- test-front/index.js
+++ test-front/index.js
@@ -3,11 +3,13 @@ form.addEventListener("submit", handleSubmit);
 
 async function handleSubmit(e) {
   e.preventDefault();
+  const formData = new FormData(form);
   let response = await fetch('http://localhost:3000/data', {
     method: 'POST',
-    body: new FormData(form),
+    body: formData,
   });
 
   let result = await response.json();
   console.log(result);
+ 
 }
